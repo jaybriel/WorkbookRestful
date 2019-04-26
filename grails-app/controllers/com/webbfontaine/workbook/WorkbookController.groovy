1b391ba0b102b70b894a8d967e6d68feb860bd95
Jaybriel Somcio <jaybriel.somcio@webbfontaine.com>
2019-04-26 19:37:56 +0800

    #Restful in controller

--- grails-app/controllers/com/webbfontaine/workbook/WorkbookController.groovy
+++ grails-app/controllers/com/webbfontaine/workbook/WorkbookController.groovy
@@ -1,6 +1,10 @@
 package com.webbfontaine.workbook
 
 import grails.gorm.transactions.*
+import groovy.json.JsonSlurper
+import org.omg.PortableInterceptor.SUCCESSFUL
+import org.springframework.http.HttpStatus
+
 import static org.springframework.http.HttpStatus.*
 import static org.springframework.http.HttpMethod.*
 
@@ -10,35 +14,27 @@ class WorkbookController {
     def workbookService
     def sessionService
     def messageSource
+    def restClientService
 
-
-    static allowedMethods = [index:"GET",save: "POST", update: "PUT", delete: "DELETE"]
+//    static allowedMethods = [index:"GET",save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+    static responseFormats = ['json', 'xml']
     def index() {
-        flash.message = "Welcome!"
-        def workbookList = workbookService.list()
-
+        def respond = restClientService.getHttpMethod("/index")
+        println "status : " + respond.getStatus()
+        def workbookList = new JsonSlurper().parseText(respond.body)
+//        render view: 'restList', model: [workbooklist: workbooklist]
 
         render(model: [workbookList: workbookList], view: 'index')
-
-
     }
 
-    def show(Workbook workbook) {
-        if(workbook == null) {
-            render status:404
-        }
-        else {
-            return [workbook: workbook]
-        }
-    }
+    def show(long id) {
+        def respond = restClientService.getHttpMethod("/show/${id}")
 
-    def view(Long id) {//show
-        def workbook = workbookService.retrieveWorkbookById(id)
-
+        def workbook = new JsonSlurper().parseText(respond.body)
         render(view: '/workbook/show', model: [workbook: workbook, workplaceList: workbook.workplaces, actionName: actionName])
-
-
     }
+
 
 
     def createWorkbook() {
@@ -52,45 +48,58 @@ class WorkbookController {
     }
 
 
-    def delete(Long id) {
-        workbookService.delete(id)
-        redirect action: "index"
+    def delete() {
+        def respond = restClientService.deleteHttpMethod("/delete/${params.id}")
+        if(respond.getStatus()==HttpStatus.OK.value()){
+//            def workbook = new JsonSlurper().parseText(respond.body)
+//            render(model: [workplaceList: workbook.workplaces, workbookList: workbook.findAll(), errorBean: workbook, sessionId: sessionId], view: '/workbook/index')
+            redirect(action:"index")
 
+        }else{
+//            render(model: [workbook: workbook, errorBean: workbook, actionName: 'createWorkbook', sessionId: sessionId], view: '/workbook/create')
 
+        }
     }
 
     def edit(Long id) {
+        def respond = restClientService.getHttpMethod("/show/${params.id}")
+        def workbook = new JsonSlurper().parseText(respond.body)
+        render(view: '/workbook/create', model: [workbook: workbook, workplaceList: workbook.workplaces, actionName: actionName])
+    }
 
-        def workbook = workbookService.retrieveWorkbookById(id)
-        def sid = session.id
-        sessionService.addToSessionStore(sid, workbook)
-        sessionService.getObjectFromSession(sid)
-        params.sessionId = sid
-        if (workbook) {
-            render(view: '/workbook/create', model: [workbook: workbook, workplaceList: workbook.workplaces, actionName: actionName, sessionId: params.sessionId], id: sid)
-        } else {
-            redirect action: 'index'
+    def update(){
+        def sessionId = params.sessionId
+        def respond = restClientService.putHttpMethod("/update/${params.id}", params)
+        if(respond.getStatus()==HttpStatus.OK.value()){
+//            def workbook = new JsonSlurper().parseText(respond.body)
+//            render(model: [workplaceList: workbook.workplaces, workbookList: workbook.findAll(), errorBean: workbook, sessionId: sessionId], view: '/workbook/index')
+            redirect(action:"index")
+
+        }else{
+//            render(model: [workbook: workbook, errorBean: workbook, actionName: 'createWorkbook', sessionId: sessionId], view: '/workbook/create')
+
         }
     }
 
 
 
-
     def save() {
-
-
         def sessionId = params.sessionId
-        def workbook = workbookService.retrieveWorkbookBySessionId(params.sessionId)
+        def respond = restClientService.postHttpMethod("/save", params)
 
-        workbook.clearErrors()
-        bindData(workbook, params)
-        workbookService.save(workbook,request)
-        if (!workbook.hasErrors()) {
-            render(model: [workplaceList: workbook.workplaces, workbookList: workbook.findAll(), errorBean: workbook, sessionId: sessionId], view: '/workbook/index')
+        if(respond.getStatus()==HttpStatus.OK.value()){
 
-        } else {
+//            render(model: [workplaceList: workbook.workplaces, workbookList: workbook.findAll(), errorBean: workbook, sessionId: sessionId], view: '/workbook/index')
+//            redirect(action:"index")
+            redirect(action:"index")
+
+        }else{
+            def workbook = new JsonSlurper().parseText(respond.body)
             render(model: [workbook: workbook, errorBean: workbook, actionName: 'createWorkbook', sessionId: sessionId], view: '/workbook/create')
-    }
+//             respond(workbook.errors,model: [workplaceList: workbook.workplaces, workbookList: workbook.findAll(),errorBean: workbook, sessionId: sessionId], view: '/workbook/create')
+
+
+        }
 
     }
 
